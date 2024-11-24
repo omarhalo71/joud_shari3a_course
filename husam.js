49ff5856c21e52ad0fa267f089d3fd3eb68e24ed
omarhalo71 <123020740+omarhalo71@users.noreply.github.com>
2024-11-24 18:48:31 +0300

    Update husam.js

--- husam.js
+++ husam.js
@@ -4,6 +4,11 @@ const vid1hh = document.getElementById("vid1hh");
 const vid1back = document.getElementById("vid1back");
 
 
+const vid2h = document.getElementById("vid2h");
+const vid2hh = document.getElementById("vid2hh");
+const vid2back = document.getElementById("vid2back");
+
+
 vid1h.addEventListener("click", function () {
     vid1hh.classList.add("db");
     vid1hh.classList.remove("dn");
@@ -13,6 +18,18 @@ vid1h.addEventListener("click", function () {
 vid1back.addEventListener("click", function () {
     vid1hh.classList.remove("db");
     vid1hh.classList.add("dn");
+    vid1.classList.remove("dn");
+   
+});
+vid2h.addEventListener("click", function () {
+    vid2hh.classList.add("db");
+    vid2hh.classList.remove("dn");
+    vid1.classList.add("dn");
+   
+});
+vid2back.addEventListener("click", function () {
+    vid2hh.classList.remove("db");
+    vid2hh.classList.add("dn");
     vid1.classList.remove("dn");
    
 });
